544a38928693cb524e8ddf0d3f9f396ed92152c4
Sonia Ortiz <sonia.ortiz@sephora.com>
2022-02-10 11:31:01 -0800

    [SERV-0002]: added footer, social media

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { Header } from './components/Header';
+import { Header } from './components/header/Header';
 import theme from './theme';
 import { ThemeProvider }from '@mui/material/styles';
 
