2e52c701ba595140f634994edee9d76312e58f83
Sonia Ortiz <sonia.ortiz@sephora.com>
2022-02-10 16:18:43 -0800

    [SERV-0005]: added company alt description

--- src/components/welcome/Welcome.jsx
+++ src/components/welcome/Welcome.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
 
-
 export function Welcome() {
     return (
         <div>
             <h1>EMPRESA</h1>
-            <img src='pexels-akil-mazumder-1072824-small.jpg' />
+            <img src='pexels-akil-mazumder-1072824-small.jpg' alt='growing plant in hand'/>
             <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iste error quasi, tempore ut labore nostrum nulla, aut quo corrupti, ea velit accusamus veritatis praesentium obcaecati tempora veniam quas inventore sint?</p>
             <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis corporis aperiam nobis quos odio voluptatibus molestias maxime provident excepturi, fugiat unde labore, debitis nemo, commodi itaque in? A, explicabo earum!</p>
         </div>
