504305e14e552e273c1abb7d6e68c22c7b8fe907
Sonia <ortizt.sonia@gmail.com>
2022-03-29 21:30:10 -0700

    changed style for welcome page

--- src/components/welcome/Welcome.jsx
+++ src/components/welcome/Welcome.jsx
@@ -1,56 +1,88 @@
-import React from 'react';
+import React from "react";
 import Box from "@mui/material/Box";
+import { Typography } from "@mui/material";
 
 export function Welcome() {
-    return (
-        <div
-            style={
-                {
-                    // backgroundColor: 'blue'
-                }
-            }
-        >
-            <h1>Servicios Biotecnológicos del Guadiana S.A de C.V</h1>
-            <Box
-                sx={{
-                    mr: 2,
-                    display: { xs: "none", md: "flex" },
-                    margin: 'auto',
-                    width: '50%',
-                    height: '50%'
+  return (
+    <div
+      style={
+        {
+          // backgroundColor: 'blue'
+          // color: "green"
+          // width: "50%"
+        }
+      }
+    >
+      <Typography variant="h1" component="h2">
+        Servicios Biotecnológicos del Guadiana S.A de C.V
+      </Typography>
+      <Box
+        sx={{
+          mr: 2,
+          display: { xs: "none", md: "flex" },
+          margin: "auto",
+          width: "50%",
+          height: "50%",
+          //   border: "solid",
+          paddingTop: "2%",
+          paddingBottom: "2%"
+        }}
+      >
+        <img src="pexels-akil-mazumder-1072824-small.jpg" alt="my alt text" />
+      </Box>
 
-                }}
-            >
-                <img src='pexels-akil-mazumder-1072824-small.jpg' alt='my alt text' />
-            </Box>
+      <Box
+        sx={{
+          mr: 2,
+          display: { lg: "none", md: "none" }
+          // backgroundColor: 'red'
+        }}
+      >
+        <img
+          src="pexels-akil-mazumder-1072824-small.jpg"
+          alt="lkfdsjlk"
+          style={{
+            // margin: "auto",
+            width: "100%",
+            height: "100%",
+            padding: "20%",
+            border: "solid"
+          }}
+        />
+      </Box>
 
-            <Box
-                sx={{
-                    mr: 2,
-                    display: { lg: "none", md: "none" },
-                    // backgroundColor: 'red'
-
-                }}
-            >
-                <img src='pexels-akil-mazumder-1072824-small.jpg'
-                    alt='lkfdsjlk'
-                    style={{
-                        margin: 'auto',
-                        width: '100%',
-                        height: '100%'
-                    }} />
-            </Box>
-
-            <h2>Quienes somos?</h2>
-            <p>
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iste error quasi, tempore ut labore nostrum nulla, aut quo corrupti, ea velit accusamus veritatis praesentium obcaecati tempora veniam quas inventore sint?
-                </p>
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dignissimos aliquam fugiat quae, animi maxime saepe eveniet, veniam eaque odit vel placeat. Id qui consequatur velit reprehenderit suscipit debitis nihil ea?</p>
-            <h2>Otra descipción</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis corporis aperiam nobis quos odio voluptatibus molestias maxime provident excepturi, fugiat unde labore, debitis nemo, commodi itaque in? A, explicabo earum!</p>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis corporis aperiam nobis quos odio voluptatibus molestias maxime provident excepturi, fugiat unde labore, debitis nemo, commodi itaque in? A, explicabo earum!</p>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis corporis aperiam nobis quos odio voluptatibus molestias maxime provident excepturi, fugiat unde labore, debitis nemo, commodi itaque in? A, explicabo earum!</p>
-
-        </div>
-    );
+      {/* <Typography variant="h3">Quienes somos?</Typography>
+      <Typography paragraph={true}>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iste error
+        quasi, tempore ut labore nostrum nulla, aut quo corrupti, ea velit
+        accusamus veritatis praesentium obcaecati tempora veniam quas inventore
+        sint?
+      </Typography>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dignissimos
+        aliquam fugiat quae, animi maxime saepe eveniet, veniam eaque odit vel
+        placeat. Id qui consequatur velit reprehenderit suscipit debitis nihil
+        ea?
+      </p>
+      <h2>Otra descipción</h2>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis
+        corporis aperiam nobis quos odio voluptatibus molestias maxime provident
+        excepturi, fugiat unde labore, debitis nemo, commodi itaque in? A,
+        explicabo earum!
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis
+        corporis aperiam nobis quos odio voluptatibus molestias maxime provident
+        excepturi, fugiat unde labore, debitis nemo, commodi itaque in? A,
+        explicabo earum!
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis
+        corporis aperiam nobis quos odio voluptatibus molestias maxime provident
+        excepturi, fugiat unde labore, debitis nemo, commodi itaque in? A,
+        explicabo earum!
+      </p> */}
+    </div>
+  );
 }
